e505fbcbde4eba050a5bd70e27c2d926c83072e3
tgetman <tjg01@me.com>
2017-06-24 16:10:11 -0400

    Initial Code
    pre google drug search addition

---
+++ genericDrug.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import wikipedia
+
+brandName = raw_input("Enter brand name of drug: ")
+
+def search (brandName):
+
+	search = wikipedia.search(brandName, results=1)
+
+	genericPage = wikipedia.page(search)
+
+	genericName = genericPage.title
+	print(genericName)
+
