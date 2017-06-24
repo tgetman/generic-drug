2391ba4774a7c40aecf7e6f9b6641e1fc9863c8b
tgetman <tjg01@me.com>
2017-06-24 19:16:09 -0400

    no more google

--- genericDrug.py
+++ genericDrug.py
@@ -2,16 +2,31 @@
 
 import wikipedia
 
-brandName = raw_input("Enter brand name of drug: ")
+error = int()
 
-def search (brandName):
+def genericDrug():
+
+	brandName = raw_input("Enter brand name of drug: ")
 
 	search = wikipedia.search(brandName, results=1)
 
 	genericPage = wikipedia.page(search)
 
-	genericName = genericPage.title
-	print(genericName)
+
+	def response (genericPage):
+		genericName = genericPage.title
+		print(genericName)
 	
-search(brandName)
+	summarytext = wikipedia.summary(genericPage)
+
+	if summarytext.count("drug") >= 1 :
+		response(genericPage)
+		error = 0
+	else:
+		error = 1
+		print("an error occured, please try again")	
+while True:		
+	genericDrug()
+
+
 
